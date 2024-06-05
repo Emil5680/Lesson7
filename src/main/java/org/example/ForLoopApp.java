70084f5958f0c056d2f01661c4820a6b85c4239a
emilc <emil.cebrayilov17@mail.ru>
2024-06-05 16:59:19 +0400

    ForLoopApp

--- src/main/java/org/example/ForLoopApp.java
+++ src/main/java/org/example/ForLoopApp.java
@@ -1,2 +1,12 @@
-package org.example;public class ForLoopApp {
+package org.example;
+
+public class ForLoopApp {
+    public static void main(String[] args) {
+        for (int i = 7; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/main/java/org/example/Task3.java
+++ src/main/java/org/example/Task3.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.sql.SQLOutput;
+
 public class Task3 {
     public static void main(String[] args) {
-        
+        for(int i=7; i>6; i--){
+            for(int j=0; j<i; j++){
+                System.out.println();
+            }
+        }
     }
 }
